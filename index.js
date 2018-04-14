3d0d78914a4c632b2fa370419e84fbdb5bd6617f
Shahar Talmi <shahar.talmi@gmail.com>
2018-04-14 15:23:49 +0300

    executable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const Generator = require('generator-wix-js/generators/app/index');
 const env = require('yeoman-environment').createEnv();
 new Generator(process.argv.slice(2), {env, resolved: require.resolve('generator-wix-js/generators/app/index')}).run();
