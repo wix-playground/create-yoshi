3dac183b3f787251bc7376ff7c920b69328e3e77
Shahar Talmi <shahar.talmi@gmail.com>
2018-04-14 21:43:10 +0300

    added error handling

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const env = require('yeoman-environment').createEnv();
 
 console.log('');
+
+env.on('error', err => {
+    console.error(err.message);
+    process.exit(err.code || 1);
+});
 env.register(require.resolve('generator-wix-js/generators/app/index'));
 env.run(['wix-js', ...process.argv.slice(2)].join(' '), {'skip-update-check': true});
 
