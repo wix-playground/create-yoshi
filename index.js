88a310681a1f127bbc50812bbecd9f78a0384991
Shahar Talmi <shahar.talmi@gmail.com>
2018-04-14 21:44:05 +0300

    lint fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const env = require('yeoman-environment').createEnv();
-
 console.log('');
 
+const env = require('yeoman-environment').createEnv();
 env.on('error', err => {
-    console.error(err.message);
-    process.exit(err.code || 1);
+  console.error('');
+  console.error(err.message);
+  process.exit(err.code || 1);
 });
 env.register(require.resolve('generator-wix-js/generators/app/index'));
 env.run(['wix-js', ...process.argv.slice(2)].join(' '), {'skip-update-check': true});
